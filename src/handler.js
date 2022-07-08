6ec3a7638da170c493cf5e568696a463c14cfc09
irvan taufik <irvantaufik28@gmail.com>
2022-07-08 15:42:40 +0700

    tampilkan

--- src/handler.js
+++ src/handler.js
@@ -1,43 +1,52 @@
 const { nanoid } = require('nanoid');
-const notes = require("./notes");
+const notes = require('./notes');
+
+
 
 const addNoteHandler = (request, h) => {
-
     const { title, tags, body } = request.payload;
-
+   
     const id = nanoid(16);
-    const createAt = new Date().toISOString();
-    const updateAt = createdAt;
-
+    const createdAt = new Date().toISOString();
+    const updatedAt = createdAt;
+   
     const newNote = {
-        title, tags, body, id, createAt, updateAt,
+      title, tags, body, id, createdAt, updatedAt,
     };
-
+   
     notes.push(newNote);
-
-};
-
-
-const isSuccess = note.filter((note) => note.id === id).length > 0;
-
-if (isSuccess) {
-    const response = h.response({
+   
+    const isSuccess = notes.filter((note) => note.id === id).length > 0;
+   
+    if (isSuccess) {
+      const response = h.response({
         status: 'success',
         message: 'Catatan berhasil ditambahkan',
         data: {
-            noteID: id,
+          noteId: id,
         },
+      });
+      response.code(201);
+      return response;
+    }
+    const response = h.response({
+      status: 'fail',
+      message: 'Catatan gagal ditambahkan',
     });
-    response.code(201);
+    response.code(500);
     return response;
-}
 
-const response = h.response({
-    status: 'fail',
-    message: 'Catatan gagal ditambahkan',
-});
 
-response.code(500);
-return response;
+  };
 
-module.exports = { addNoteHandler };
+
+  const getAllNotesHandler = ()=> ({
+    status : 'success',
+    data: {
+        notes,
+    },
+  });
+
+ 
+
+module.exports = { addNoteHandler, getAllNotesHandler }
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,21 @@
+const { addNoteHandler, getAllNotesHandler } = require('./handler');
+
 const routes = [
     {
-        method : 'POST',
-        path: './notes',
-        handler: () =>{},
+        method: 'POST',
+        path: '/notes',
+         handler: addNoteHandler,
     },
+    {
+        method: 'GET',
+        path: '/notes',
+        handler: getAllNotesHandler,
+    },
+    {
+        method : 'GET',
+        path: '/notes/{id}',
+        handler: () => {},
+    }
 ];
 
 module.exports = routes;
